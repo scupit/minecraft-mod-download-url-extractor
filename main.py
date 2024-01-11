2f7d3ed897ceb433bb7b9118aac7978dd6b42724
Skylar Cupit <Skylarcupit@gmail.com>
2024-01-11 01:33:24 -0600

    Initial version working

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import sys
+import requests
+from bs4 import BeautifulSoup
 
-if len(sys.argv) > 1:
-  print("Additional arguments: ")
-  for x in range(1, len(sys.argv)):
-    print("\t", sys.argv[x])
-else:
-  print("No additional arguments passed to python script...")
+def getModPage(modName: str, versionString: str) -> str:
+  return requests.get(f"https://modrinth.com/mod/{modName}/versions", params={
+    "g": versionString,
+    "l": "fabric"
+  }).text
+
+def modDownloadUrl(modName: str, version: str) -> str | None:
+  pageHtml = BeautifulSoup(getModPage(modName, version), features="html.parser")
+  versionsTable = pageHtml.find(id="all-versions")
+
+  if versionsTable is None:
+    return None
+  else:
+    linkTag = versionsTable.find("a", class_="download-button")
+    return linkTag.attrs["href"]
+
+testValid = modDownloadUrl("nvidium", "1.20.3")
+testInvalid = modDownloadUrl("nvidium", "1.20.12")
+
+print(testValid)
+print(testInvalid)
